6f3e68a04fdfd6e48c266fa0d84bacb54223b031
Dragmine <dan@thebanners.uk>
2022-01-12 20:52:58 +0000

    First iteration of the bot

---
+++ index.ts
@@ -0,0 +1,40 @@
+import DiscordJS, {Intents, TextChannel} from 'discord.js'
+import dotenv from 'dotenv'
+import WOKCommands from 'wokcommands'
+import path from 'path'
+
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILDS
+    ],
+})
+
+client.on('ready', async () => {
+    console.log('Bot is ready')
+
+    new WOKCommands(client, {
+        commandsDir: path.join(__dirname, 'commands'),
+        typeScript: true,
+        testServers: ["686177483430952970"],
+        botOwners: ['467718535897022479'],
+        disabledDefaultCommands: [
+            'help',
+            'command',
+            'language',
+            'prefix',
+            'requiredrole',
+            'channelonly'
+        ]
+    })
+
+    await new Promise(resolve => setTimeout(resolve, 1.25 * 1000))
+
+    const messageChannel = client.channels.cache.get('930916536301011005') as TextChannel
+    messageChannel.send(`Bot is ready - ${new Intl.DateTimeFormat('en-GB', { "timeStyle": "medium" }).format(new Date)}`)
+})
+
+client.login(process.env.TOKEN);
